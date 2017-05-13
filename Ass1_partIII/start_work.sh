1807e8c9abe4db93b391126d332899aa07737c13
taineleau <taineleau@gmail.com>
2017-05-13 00:11:06 -0400

    fix typo

--- Ass1_partIII/start_work.sh
+++ Ass1_partIII/start_work.sh
@@ -1,6 +1,6 @@
 mkdir ../../prml_ass1_partIII
 cp *.py *.sh *.tex *.bib ../../prml_ass1_partIII
-cd ../../prml_ass1
+cd ../../prml_ass1_partIII
 git init
 git add .
 git commit -m "initial init for ass 1 part III"
